c4cddf379a181dd8b0ea42ecc563b528ad96ae50
Tu Nombre <tu@email.com>
2025-10-24 14:22:19 -0500

    Fix: small adjustments in the Transaction entity

--- src/main/java/com/gestock/GestockBackend/model/entity/Transaction.java
+++ src/main/java/com/gestock/GestockBackend/model/entity/Transaction.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@Entity
+@Table(name = "transactions")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "transactions")
 public class Transaction {
 
     @Id
